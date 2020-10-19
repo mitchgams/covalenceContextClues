931fc09fd6413c3534a5da246154bfc8200cd87f
Mitchell Gams <mitchgams@gmail.com>
2020-10-19 15:38:05 -0500

    blah

--- app.js
+++ app.js
@@ -3,20 +3,17 @@ var locations = ['bathroom', 'library', 'patio', 'balcony', 'pool', 'study', 'ma
 var weapons = ['toothpick', 'pencil', 'hammer', 'potato', 'rock', 'lead pipe', 'broom', 'chicken', 'brick', 'stick',
                 'leaf blower', 'baseball bat', 'fishing pole', 'fingernail clippers', 'spatchula', 'frisbe', 'usb-c cable', 'grass clipping', 'orange', 'carrot'];
 $(document).ready(function() {
-    for(let i = 1; i < 101; i++) {
+    for(var i = 1; i < 101; i++) {
         $('<li class="list-group-item"><h3 id=' + i + '>Accusation ' + i + '</h3></li>').appendTo('.list-group-flush');
-        //$('<h3 id=' + i + '>Accusation ' + i + '</h3>').appendTo('body');
-        $('#'+i).click(clickH3(i));
+        $('h3').click(clickH3(i));
     }
 });
 
-function clickH3(i) {
-    var friend = friends[Math.floor(i/20)];
-    var location = locations[Math.floor(i/10)];
-    var weapon = weapons[Math.floor(i/5)];
-
-     function accuse() {
-        alert("Accusation " + i + ": I accuse " + friend + ", with the " + weapon + " in the " + location + "!");
+function clickH3(num) {
+    var friend = friends[Math.floor(num/20)];
+    var location = locations[Math.floor(num/10)];
+    var weapon = weapons[Math.floor(num/5)];
+    return function() {
+        alert("Accusation " + num + ": I accuse " + friend + ", with the " + weapon + " in the " + location + "!");
     }
-    return accuse;
 }
