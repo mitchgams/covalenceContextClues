48b8bc914e7535ae3b171cda3797845f94d40d68
Mitchell Gams <mitchgams@gmail.com>
2020-10-19 14:54:42 -0500

    added some bootstrap

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var weapons = ['toothpick', 'pencil', 'hammer', 'potato', 'rock', 'lead pipe', '
                 'leaf blower', 'baseball bat', 'fishing pole', 'fingernail clippers', 'spatchula', 'frisbe', 'usb-c cable', 'grass clipping', 'orange', 'carrot'];
 $(document).ready(function() {
     for(let i = 1; i < 101; i++) {
-        $('<h3 id=' + i + '>Accusation ' + i + '</h3>').appendTo('body');
+        $('<li class="list-group-item"><h3 id=' + i + '>Accusation ' + i + '</h3></li>').appendTo('.list-group-flush');
+        //$('<h3 id=' + i + '>Accusation ' + i + '</h3>').appendTo('body');
         $('#'+i).click(clickH3(i));
     }
 });
